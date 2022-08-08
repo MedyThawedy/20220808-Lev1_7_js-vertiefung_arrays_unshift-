d2a1d5517b513d781987ee244788b5d96aa0c763
MD <dhmedy@googlemail.com>
2022-08-08 14:15:10 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// Definiere deutscheGerichte (die du kennen solltest) mit einem Array, das enthält: 
+// Speckkuchen, Thüringer Rostbratwurst, Quarkkeulchen, Sauerbraten.
+
+let deutscheGerichte = ["Speckkuchen", "Thürigner Rostbratwurst", "Quarkkeulchen", "Sauerbraten"];
+console.log(deutscheGerichte);
+
+// Füge zum Array 5 deutsche Gerichte mit dem Befehl unshift() hinzu.
+// "Brezel", "Kartoffelsalat", "Kaiserschmarrn", "Käsekuchen", "Berliner"
+deutscheGerichte.unshift("Brezel", "Kartoffelsalat", "Kaiserschmarrn", "Käsekuchen", "Berliner");
+
+console.log(deutscheGerichte);
